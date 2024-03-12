a7b92530ed866c8be96cd71d9776ee0ebfa2c3f2
Matthieu Petruzzi <matthieu.petruzzi@gmail.com>
2024-03-12 13:37:09 +0100

    project init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import './crypto-polyfill.js'
+import '@walletconnect/react-native-compat'
+import { W3mButton, W3mAccountButton } from '@web3modal/wagmi-react-native'
+import { View, StyleSheet, Pressable, Text } from 'react-native'
+import { WagmiConfig } from 'wagmi'
+import { mainnet, polygon, arbitrum } from 'viem/chains'
+import {
+  createWeb3Modal,
+  defaultWagmiConfig,
+  Web3Modal,
+  useWeb3Modal,
+} from '@web3modal/wagmi-react-native'
+
+const projectId = 'xxxxxx'
+
+const metadata = {
+  name: 'Web3Modal RN',
+  description: 'Web3Modal RN Example',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886'],
+  redirect: {
+    native: 'YOUR_APP_SCHEME://',
+    universal: 'YOUR_APP_UNIVERSAL_LINK.com',
+  },
+}
+
+const chains = [mainnet, polygon, arbitrum]
+
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+
+createWeb3Modal({
+  projectId,
+  chains,
+  wagmiConfig,
+})
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <WagmiConfig config={wagmiConfig}>
+      <Web3Modal />
+      <View style={styles.centeredView}>
+        <W3mButton />
+        {/* <W3mButton /> */}
+      </View>
+    </WagmiConfig>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  centeredView: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
   },
-});
+})
